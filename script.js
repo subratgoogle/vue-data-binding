4d1a76454b402375123c07cd49de685f10a0ad46
subrat-google <subratverma3356@gmail.com>
2019-09-03 13:37:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var myapp = new Vue({
+
+    el: "#my-app",
+    data:{
+        // start html attribute binding 
+      name : "React Js Tutorial",  //data binding 
+      title: "This is channel link",
+      imageLink: "a.jpg",  
+      isActive: true, //these are boolian for activate anything 
+      ulink: "https://www.youtube.com/watch?v=zKkUN-mJtPQ&list=PL6n9fhu94yhWKHkcL7RJmmXyxkuFB3KSl",
+     //end html attribute binding 
+     //form input binding 
+     txtName:"Subrat Kumar Soni",
+     txtEmail: "user123@gmail.com",
+//html content binding 
+htmlContent: "<p>I am subrat kumar soni form ghaziabad uttar pradesh</p>"
+    
+    }
+   
+})
